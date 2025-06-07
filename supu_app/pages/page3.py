62847889414ce98980f150b3f5d764ecd3ae471d
Makoto-Kobayashi-001 <nekokappa@cma.bbiq.jp>
2025-06-08 02:40:16 +0900

    Add files via upload

--- supu_app/pages/page3.py
+++ supu_app/pages/page3.py
@@ -5,8 +5,8 @@ import pandas as pd
 import os
 
 #データ分析
-st.text(os.path.abspath('./data/sample.csv'))
-df = pd.read_csv(os.path.abspath('./data/sample.csv'),index_col='月')
+st.text(os.path.abspath('./supu_app/data/sample.csv'))
+df = pd.read_csv(os.path.abspath('./supu_app/data/sample.csv'),index_col='月')
 st.dataframe(df)
 st.table(df)
 st.line_chart(df)
